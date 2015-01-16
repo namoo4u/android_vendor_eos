NamelessRom/android_vendor_nameless

themes: Build new ThemesProvider
Change-Id: Ia309407d94ceb38d56b899d1801944d5bda1acda

config/themes_common.mk
@@ -1,9 +1,8 @@
-# T-Mobile theme engine
+# Theme engine
 PRODUCT_PACKAGES += \
-       ThemeManager \
-       ThemeChooser \
-       com.tmobile.themes
+    ThemeChooser \
+    ThemesProvider
 
 PRODUCT_COPY_FILES += \
-       vendor/nameless/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
-       vendor/nameless/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
+    vendor/nameless/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/nameless/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml

namoo4u/android_vendor_eos

config/themes_common.mk
@@ -1,9 +1,8 @@
-# T-Mobile theme engine
+# Theme engine
 PRODUCT_PACKAGES += \
-       ThemeManager \
-       ThemeChooser \
-       com.tmobile.themes
+    ThemeChooser \
+    ThemesProvider
 
 PRODUCT_COPY_FILES += \
-       vendor/cm/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
-       vendor/cm/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
+    vendor/eos/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/eos/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml